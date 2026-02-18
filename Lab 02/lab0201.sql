c3644bf82b9c4456b45c0e9907ef32ed4287c007
3PI70M3 <74763133+aviskarrr@users.noreply.github.com>
2026-02-18 08:37:15 +0545

    final push

--- Lab 02/lab0201.sql
+++ Lab 02/lab0201.sql
@@ -197,7 +197,7 @@ WHERE bname  = 'Maths';
 
 select * from book;
 
--- task 10 : provide 5% increment to all salaries whose salary is greater than 20k and 20% increment in rest of slaaries.
+-- task 9 : provide 5% increment to all salaries whose salary is greater than 20k and 20% increment in rest of slaaries.
 UPDATE employee
 SET salary = CASE
     WHEN salary > 20000 THEN salary * 1.05
@@ -237,14 +237,20 @@ select * from booklist;
 
 SELECT b.bname, b.author
 FROM book b
-WHERE b.bid IN (
+WHERE b.bid in (
     SELECT bl.isbn
     FROM booklist bl
     WHERE bl.pub = 'NepalPub'
 );
 
 
-
+select * from student;
+select * from teacher;
+select * from booklist;
+select * from employee;
+select * from teacher;
+select * from book;
+select * from issue;
 
 ----lab04---------lab04---------lab04---------lab04---------lab04---------lab04---------lab04-----
 
@@ -256,11 +262,15 @@ FROM employee
 ORDER BY salary DESC;
 
 
--- Task 2: Sort name and publication name in ascending order.
 
+-- Task 2: Sort name and publication name in ascending order.
+select * from booklist;
 SELECT name, pub
 FROM booklist
 ORDER BY name ASC, pub ASC;
+
+
+select ename from employee order by ename asc;
 
 --Task 3: Display atop three records from teachers relation
 SELECT TOP 3 *
@@ -270,3 +280,133 @@ FROM teacher;
 -- Task 4: Display the sum of salaries all employees.
 SELECT SUM(salary) AS total_salary
 FROM employee;
+
+
+
+---lab05....------
+
+--Task 5: Display min salary of employee
+select MIN(salary) as min_sal from employee;
+;
+
+
+-- Task 6: Display avg price of book written by same author
+
+SELECT author, AVG(price) AS avg_price
+FROM book
+GROUP BY author;
+
+-- Task 7: Display publication name and number of books published by it from book list relation publication wise.
+
+SELECT pub, COUNT(*) AS total_books
+FROM booklist
+GROUP BY pub;
+
+
+-- Task 8: Display the bid and bname of books whose price is greater than avg price of books.
+SELECT bid, bname
+FROM book
+WHERE price > (
+    SELECT AVG(price)
+    FROM book
+);
+
+
+select bid, bname from book where price > (select avg(price) from book)
+
+-- Task 9: Find the bid, bname and author in ascending order where author name is started by 's'
+SELECT bid, bname, author
+FROM book
+WHERE author LIKE 'S%'
+ORDER BY author ASC;
+
+
+select bid, bname, author from book where author like 's%' order by author asc 
+
+
+---lab06
+select * from teacher;
+select * from book;
+
+-- 10. Find the teachers name and book taken by him. The teacher’s salary who takes the book
+--should be the maximum salary.
+
+SELECT t.tname, b.bname
+FROM teacher t
+JOIN book b ON b.author = t.tname
+WHERE t.salary = (SELECT MAX(salary) FROM teacher);
+
+
+-- 11. Find the authors name who have written more than one book.
+insert into book (bid, bname, author, price) values (1005,'AI', 'Mohan', 5000);
+
+WHERE bid = 1005;
+select author from book group by author having COUNT(*) > 1;
+ select * from book
+
+
+ --lab05 questions
+ --1 Perform join operation on teacher and employee table and display the Ename , Faculty and salary
+ select * from employee join teacher on employee.eid = teacher.tid;
+select ename, employee.salary from employee join teacher on employee.eid= teacher.tid;
+
+
+--2. Perform left join on table book list and book table.
+select * from booklist left join book on booklist.isbn = book.bid;
+
+select * from booklist
+select * from book
+-- 3 Perform right join on booklist and book table
+select * from booklist right join book on booklist.isbn = book.bid;
+
+
+-- 4. Perform Full join on book and booklist table
+select * from student
+select * from issue
+
+select * from booklist full JOIN book on booklist.isbn = book.bid;
+
+--5. Display those employees name and salary whose name starts with ‘s’ and whose name consist ‘a’ as sub string
+SELECT ename, salary FROM employee WHERE ename LIKE 'M%' AND ename LIKE '%a%';
+
+
+-- 6. Display name of the employee who is also a teacher.
+select ename from employee full join teacher on teacher.tid = employee.eid where teacher.tname = employee.ename;
+
+-- 7. Display all employees name except the name who are teachers
+select ename from employee full join teacher on teacher.tid  = employee.eid where teacher.tname != employee.ename;
+
+select ename from employee where ename not in  (select tname from teacher);
+select * from employee
+select * from teacher
+
+
+-- 8. Create a view Employee-view which consist of eid, ename , salary as attributes.
+create view teast as 
+select eid, ename, salary from employee;
+
+select * from teast
+
+-- 9. Insert a new record in recently created view. And also display the contents of primary table.
+INSERT INTO teast (eid, ename, salary)
+VALUES (3, 'Ramesh', 45000);
+
+select * from employee;
+
+-- 10. selete the information from view where salary are less than 29000.
+DELETE FROM teast
+WHERE salary = 45000;
+
+
+-- procedure
+CREATE PROCEDURE okk
+AS
+BEGIN
+    SELECT * FROM book;
+END
+GO
+
+
+EXEC okk;
+
+drop procedure okk;
